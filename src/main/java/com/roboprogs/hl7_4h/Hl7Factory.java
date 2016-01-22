da1355c5e4a74f7d66984761aeaf5a9c553d579a
Rob Anderson <roboprog@yahoo.com>
2016-01-21 21:23:55 -0800

    load stub of JS implementation

--- src/main/java/com/roboprogs/hl7_4h/Hl7Factory.java
+++ src/main/java/com/roboprogs/hl7_4h/Hl7Factory.java
@@ -1,14 +1,52 @@
 package com.roboprogs.hl7_4h;
 
+import java.io.*;
+
+import javax.script.ScriptEngineManager;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
 /**
  * Java wrapper around HL7-4H Javascript library.
  */
 public
 class Hl7Factory {
 
-    public static
-    void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    /** JS interpreter, loaded with any needed scripting */
+    private static
+    NashornScriptEngine engine = initEngine();
+
+    /** init JS interpreter, loaded with any needed scripting */
+    private static
+    NashornScriptEngine initEngine() {
+        NashornScriptEngine engine;
+
+        try {
+            engine = (NashornScriptEngine) ( new ScriptEngineManager() ) .
+                    getEngineByName( "nashorn" );
+            loadScript( engine, "/js/hl7_4h.js" );
+            return engine;
+        } catch ( Throwable e ) {
+            throw new RuntimeException(
+                    "Failed to initialize Javascript engine: " + e, e );
+        }
+    }
+
+    /** Run (load) the indicated Javascript (resource) */
+    private static
+    void loadScript(
+            NashornScriptEngine engine,
+            String fname )
+            throws Exception {
+        Reader in;
+
+        in = new BufferedReader( new InputStreamReader(
+                Hl7Factory.class.getResourceAsStream( fname ) ) );
+        try {
+            engine.eval( in );
+        } finally {
+            in.close();
+        }
     }
 
     /** create an HL7 message accessor from the given message text */
---
+++ src/main/resources/js/hl7_4h.js
@@ -0,0 +1,13 @@
+/**
+ * Namespace for the HL7-4H library.
+ * @type {{}}
+ */
+var hl7_4h = (
+    function () {
+        return {}  // TODO
+    }
+)()
+
+
+// vi: ts=4 sw=4 expandtab
+// *** EOF ***
